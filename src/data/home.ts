01cd1325e3424ac2b0908dc065d00eda3ff2997f
mylesshin <myles.h.shin@vanderbilt.ediu>
2024-03-26 18:19:14 -0500

    First commit

---
+++ src/data/home.ts
@@ -0,0 +1,145 @@
+export const categories = [
+    "All",
+    "Gaming",
+    "Javascript",
+    "Electric Vehicles",
+    "Los Angeles Clippers",
+    "Algorithms",
+    "Vanderbilt",
+    "Computer Hardware",
+    "Shopping",
+    "Python",
+    "React.js",
+    "Web Development"
+]
+
+export const videos = [
+    {
+      id: "1",
+      title: "lofi hip hop radio 📚 - beats to relax/study to",
+      channel: {
+        name: "Lofi Girl",
+        id: "LofiGirl",
+        profileUrl:
+          "https://yt3.ggpht.com/M0eY1tfgiwuyqrSlWIkzf5-6RZSARiuChjpXyZe-hfl9C2fn-I4leLtxKAqYqGZv_FgE4u5TKQ=s68-c-k-c0x00ffffff-no-rj",
+      },
+      views: 222536,
+      postedAt: new Date("2023-08-29"),
+      duration: 5938,
+      thumbnailUrl: "https://www.nationalworld.com/jpim-static/image/2022/07/12/14/lofi-girl-chilled-cow.jpg?width=1200&enable=upscale",
+      videoUrl:
+        "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+    },
+    {
+      id: "2",
+      title: "This Trade  Rumor Just BROKE My Brain...",
+      channel: {
+        name: "Sporting Logically",
+        id: "SportingLogically",
+        profileUrl:
+          "https://yt3.ggpht.com/ytc/AIdro_nkgt7Mw9pnW2yqmQ9k0QowiMeEhKQRKKeR8a3r1w=s68-c-k-c0x00ffffff-no-rj",
+      },
+      views: 257136,
+      postedAt: new Date("2023-08-22"),
+      duration: 813,
+      thumbnailUrl: "https://i.ytimg.com/vi/Y5cy6bUAWaE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB78e3XgC1TMlD6lu5cJiFrxHjQBQ",
+      videoUrl:
+        "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+    },
+    {
+      id: "3",
+      title: "Ultimate Guide to Starting and Growing a Smart Home in 2024",
+      channel: {
+        name: "Smart Home Solver",
+        id: "SmartHomeSolver",
+        profileUrl:
+          "https://yt3.ggpht.com/IUA9aXH6D7NRsHGH22BnpxWggopR6JgPo853KOzRr0TfDNYB7-RQlTLkuaocDgdmeoYxOXuVA6o=s68-c-k-c0x00ffffff-no-rj",
+      },
+      views: 1232300,
+      postedAt: new Date("2023-10-05"),
+      duration: 1434,
+      thumbnailUrl: "https://i.ytimg.com/vi/WHXYlEB_QmY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACGYXK8rXyYz7Rho_KoYnVzwlUVg",
+      videoUrl:
+        "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+    },
+    {
+      id: "4",
+      title: "The 997 Porsche 911 Targa Is the Quirky 997 Nobody Remembers",
+      channel: {
+        name: "Doug Demuro",
+        id: "DougDemuro",
+        profileUrl:
+          "https://yt3.googleusercontent.com/VEn5laroY5PD1B7HVUwdcuI56QAoldcecE6299L-l8KpvFkfjfoAG1cPHmh3OckNJxzaCJb1ag=s176-c-k-c0x00ffffff-no-rj",
+      },
+      views: 112,
+      postedAt: new Date("2024-03-21"),
+      duration: 4343,
+      thumbnailUrl: "https://i.ytimg.com/vi/Pr8x8MvPRIQ/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCHqaMv5qILBMHIE5xwWySf1ahFIw",
+      videoUrl:
+        "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+    },
+    {
+      id: "5",
+      title: "Are credit unions better than big banks?",
+      channel: {
+        name: "TwoCents",
+        id: "TwoCents",
+        profileUrl:
+          "https://yt3.ggpht.com/ytc/AIdro_k-ORoE9UrCVKrn-IRx2e078PbgC6u8nsk01Wlk=s68-c-k-c0x00ffffff-no-rj",
+      },
+      views: 1203453,
+      postedAt: new Date("2019-08-02"),
+      duration: 525,
+      thumbnailUrl: "https://i.ytimg.com/vi/GIMsbgIQiDE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD6mH7rVVypP0ct2VA4P2hO5QAcEgg",
+      videoUrl:
+        "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+    },
+    {
+      id: "6",
+      title: "Tesla Solar Roof vs Solar Panels: Which is Worth It?",
+      channel: {
+        name: "Undecided with Matt Ferrell",
+        id: "UndecidedwithMattFerrell",
+        profileUrl:
+          "https://yt3.googleusercontent.com/ytc/AIdro_mmJCvn5S2IfuFjxdhXQI6xfXVCK4VbEqf8vifmbw=s176-c-k-c0x00ffffff-no-rj",
+      },
+      views: 485234,
+      postedAt: new Date("2023-03-19"),
+      duration: 54,
+      thumbnailUrl: "https://i.ytimg.com/vi/glgsGjvxvz8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB8FFvYuBrERD69YMt3MwCZUZIDGQ",
+      videoUrl:
+        "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+    },
+    {
+      id: "7",
+      title: "Turning my TRUCK into a TINY HOME (for less than $1,000)",
+      channel: {
+        name: "President Chay",
+        id: "PresidentChay",
+        profileUrl:
+          "https://yt3.ggpht.com/6MHygc-lfRMNXrhh3jyCsLJ0bK1cKaIU8ZNbhqRxVTHGYLNwnj0OZVKqMIzA0LLlgwqZWJz1VQ=s68-c-k-c0x00ffffff-no-rj",
+      },
+      views: 10323340,
+      postedAt: new Date("2023-08-09"),
+      duration: 100,
+      thumbnailUrl: "https://i.ytimg.com/vi/xyeVWL0mSVw/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB61h32h-i4rTXh9FlCuJ8GqKXrPQ",
+      videoUrl:
+        "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+    },
+    {
+      id: "8",
+      title: "Vanderbilt University Campus Tour with a Student Tour Guide!",
+      channel: {
+        name: "Niche: The College Search App",
+        id: "Niche:TheCollegeSearchApp",
+        profileUrl:
+          "https://yt3.ggpht.com/wFti1jLi6B1dbNv38vILKEq6kJAiAp4j1XknPUi_osgG-ixeBSSw2SDta9aMpGWibQt4eH8fZs0=s68-c-k-c0x00ffffff-no-rj",
+      },
+      views: 20323340,
+      postedAt: new Date("2020-11-15"),
+      duration: 105,
+      thumbnailUrl: "https://i.ytimg.com/vi/Q2FfH4d_A3w/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAqp3dsR5rihqsRS3dK9rV2gpyf9A",
+      videoUrl:
+        "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+    },
+]
---
+++ src/data/sidebar.ts
@@ -0,0 +1,34 @@
+export const subscriptions = [
+    {
+      channelName: "Lofi Girl",
+      id: "LofiGirl",
+      imgUrl:
+        "https://yt3.ggpht.com/M0eY1tfgiwuyqrSlWIkzf5-6RZSARiuChjpXyZe-hfl9C2fn-I4leLtxKAqYqGZv_FgE4u5TKQ=s68-c-k-c0x00ffffff-no-rj",
+    },
+    {
+      channelName: "Doug Demuro",
+      id: "DougDemuro",
+      imgUrl:
+        "https://yt3.googleusercontent.com/VEn5laroY5PD1B7HVUwdcuI56QAoldcecE6299L-l8KpvFkfjfoAG1cPHmh3OckNJxzaCJb1ag=s176-c-k-c0x00ffffff-no-rj",
+    },
+    {
+      channelName: "President Chay",
+      id: "PresidentChay",
+      imgUrl:
+        "https://yt3.ggpht.com/6MHygc-lfRMNXrhh3jyCsLJ0bK1cKaIU8ZNbhqRxVTHGYLNwnj0OZVKqMIzA0LLlgwqZWJz1VQ=s68-c-k-c0x00ffffff-no-rj",
+    },
+    {
+      channelName: "Sporting Logically",
+      id: "SportingLogically",
+      imgUrl:
+        "https://yt3.ggpht.com/ytc/AIdro_nkgt7Mw9pnW2yqmQ9k0QowiMeEhKQRKKeR8a3r1w=s68-c-k-c0x00ffffff-no-rj",
+    },
+  ]
+  
+  export const playlists = [
+    { id: "1", name: "Cars" },
+    { id: "2", name: "React" },
+    { id: "3", name: "Lofi" },
+    { id: "4", name: "Finance" },
+    { id: "5", name: "Tech" },
+  ]
